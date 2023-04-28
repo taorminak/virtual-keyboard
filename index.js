85bb5a565d41d728c870d6dce3e0e484e53a0796
taorminak <ostanina.anna@yahoo.com>
2023-04-28 16:00:45 +0200

    feat: create basic keyboard with letters

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-var keyboard = {
-    keys: [
-      { value: "1", fn: function() { /* функция для клавиши 1 */ } },
-      { value: "2", fn: function() { /* функция для клавиши 2 */ } }
-    ]
-  };
-  
-  function renderKeyboard() {
-    var keyboardElement = document.createElement("div");
-    keyboardElement.classList.add("keyboard");
-    
-    keyboard.keys.forEach(function(key) {
-      var keyElement = document.createElement("button");
-      keyElement.textContent = key.value;
-      keyElement.addEventListener("click", key.fn);
-      keyboardElement.appendChild(keyElement);
-    });
-    
-    document.body.appendChild(keyboardElement);
+const keyboard = document.createElement('div');
+keyboard.classList.add("keyboard");
+document.body.appendChild(keyboard);
+const textarea = document.createElement('textarea');
+textarea.classList.add("textarea");
+keyboard.appendChild(textarea);
+const containerRows = document.createElement('div');
+containerRows.classList.add("container-rows");
+keyboard.appendChild(containerRows);
+const comment = document.createElement('div');
+comment.classList.add("comment");
+comment.innerHTML = "Клавиатура создана в операционной системе macOS<br/>Для переключения языка воспользуйтесь комбинацией";
+keyboard.appendChild(comment);
+
+const rows = ['1234567890', 'qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
+
+for (let i = 0; i < rows.length; i++) {
+  let row = document.createElement('div');
+  row.classList.add('row');
+  for (let j = 0; j < rows[i].length; j++) {
+    let key = document.createElement('div');
+    key.classList.add('key');
+    key.textContent = rows[i][j];
+    row.appendChild(key);
   }
   
-  renderKeyboard();
+  containerRows.appendChild(row);
+}
+
+
+function onKeyPress(event) {
+    const value = event.target.innerHTML;
+    console.log(value);
+    textarea.value += value;
+
+}
+
+const keys = keyboard.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('click', onKeyPress));
+
 
